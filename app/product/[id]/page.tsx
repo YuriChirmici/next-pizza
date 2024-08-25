6e3c55e71163ffc4313fc360f2d815d78f49fab6
Yuri Kirmichi <ykirmichi4@mail.ru>
2024-08-25 23:27:03 +0300

    add base product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+import { Container, Title } from "@/components/shared";
+import { GroupVariants } from "@/components/shared/group-variants";
+import { ProductImage } from "@/components/shared/product-image";
+import { Button } from "@/components/ui";
+import { prisma } from "@/prisma/prisma-client";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface Props {
 	params: {
@@ -6,11 +12,41 @@ interface Props {
 	}
 }
 
-export const Page: React.FC<Props> = ({ params: { id } }: Props) => {
+export default async function Page({ params: { id } }: Props) {
+	const product = await prisma.product.findFirst({
+		where: { id: Number(id) },
+		include: { items: true }
+	});
+
+	if (!product) {
+		return notFound();
+	}
+
 	return (
-		<div> Product: {id} </div>
+		<Container className="flex flex-col my-10">
+			<div className="flex flex-1">
+				<ProductImage src={product.imageUrl} size={20} name={product.name} />
+				<div className="w-[490px] bg-[#F6F6F6] p-7">
+					<Title text={product.name} size="md" className="font-extrabold mb-1" />
+					<p className="text-gray-400">Lorem ipsum dolor sit amet.</p>
+
+					<GroupVariants
+						selectedValue="1"
+						items={[ {
+							name: "Маленькая",
+							value: "1",
+						}, {
+							name: "Средняя",
+							value: "2",
+							disabled: true
+						}, {
+							name: "Большая",
+							value: "3",
+						} ]}
+					/>
+				</div>
+			</div>
+
+		</Container>
 	);
 };
-
-export default Page;
-
---
+++ components/shared/product-image.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface Props {
+	className?: string;
+	src: string;
+	name: string;
+	size: 20 | 30 | 40;
+}
+
+const sizeToPx = {
+	20: 300,
+	30: 400,
+	40: 500,
+};
+
+export const ProductImage: React.FC<Props> = ({ className, src, name, size }) => {
+	const sizePx = sizeToPx[size];
+	const circleBaseClass = "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-2 rounded-full";
+
+	return (
+		<div className={cn("flex items-center justify-center flex-1 relative w-full", className)}>
+			<img
+				src={src}
+				alt={name || "product"}
+				className={cn(
+					"relative left-2 top-2 transition-all z-10 duration-300",
+					`w-[${sizePx}px] h-[${sizePx}px]`
+				)}
+			/>
+
+			<div className={cn(circleBaseClass, "border-dashed border-gray-200 w-[320px] h-[320px]")} />
+			<div className={cn(circleBaseClass, "border-dotted border-gray-200 w-[370px] h-[370px]")} />
+		</div>
+	);
+};
